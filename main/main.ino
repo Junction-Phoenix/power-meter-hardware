2820beda0fd93c4b5f8cd15b8b61b98d446f137a
Paulius Dovidaitis <paulius.dovidaitis@gmail.com>
2022-11-05 04:05:05 +0000

    feat: relay control functionality

--- main/main.ino
+++ main/main.ino
@@ -1,17 +1,28 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
+#include <Arduino_JSON.h>
 
 const char* ssid = "ppp";                 // random for now
 const char* password = "pailius12345678"; // random for now
+const int device_id = 4;
 
-String serverName = "";
+const int relay = 19;
+
+String serverName = "https://power-manager-api.herokuapp.com";
+String get_state = "/devices/state/";
+String post_data = "/devices/consumption/";
+String response;
+
+
+int state = 0;
 
 void setup()
 {
-
+    pinMode(relay, OUTPUT);
+    
     Serial.begin(115200);
     WiFi.begin(ssid, password);
-    Serial.println("Authenticated");
+    Serial.println("Wifi authenticated");
     
     while(WiFi.status() != WL_CONNECTED) {
       delay(500);
@@ -23,9 +34,23 @@ void setup()
     Serial.println("Setup done");
 }
 
+void check_relay_state(int state){
+  if (state){
+     // if true (1), then appliance can work
+    digitalWrite(relay, HIGH);
+    }
+  else{
+    digitalWrite(relay, LOW);
+      }
+}
+
 void loop()
 {
-    Serial.println("Scan start");
+    Serial.println("Smart-power-meter running...");
+
+    check_relay_state(state);
+    
+    delay(1000);
 
   
 }
