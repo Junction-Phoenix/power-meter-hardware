2ba46c2ed782268f68e3d16afff7dd80356c9f68
Paulius Dovidaitis <paulius.dovidaitis@gmail.com>
2022-11-05 05:17:55 +0000

    feat: relay control with api

--- main/main.ino
+++ main/main.ino
@@ -1,6 +1,6 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
-#include <Arduino_JSON.h>
+
 
 const char* ssid = "ppp";                 // random for now
 const char* password = "pailius12345678"; // random for now
@@ -8,13 +8,17 @@ const int device_id = 4;
 
 const int relay = 19;
 
-String serverName = "https://power-manager-api.herokuapp.com";
-String get_state = "/devices/state/";
+unsigned long timer = 5000; // 5 seconds
+unsigned long last_time = 0;
+
+String server_name = "https://power-manager-api.herokuapp.com";
+String get_state = "/devices/state/4";
 String post_data = "/devices/consumption/";
-String response;
+String payload;
 
 
 int state = 0;
+int int_payload;
 
 void setup()
 {
@@ -34,23 +38,57 @@ void setup()
     Serial.println("Setup done");
 }
 
-void check_relay_state(int state){
-  if (state){
+void check_relay_state(String state){
+  if (state == "1"){
      // if true (1), then appliance can work
     digitalWrite(relay, HIGH);
+    Serial.println("relay HIGH");
     }
   else{
     digitalWrite(relay, LOW);
+    Serial.println("relay LOW");
       }
 }
 
 void loop()
 {
     Serial.println("Smart-power-meter running...");
-
-    check_relay_state(state);
     
-    delay(1000);
+    if ((millis() - last_time) > timer){
+      if (WiFi.status() == WL_CONNECTED){
+        HTTPClient http;
+        String full_get_state = server_name + get_state;
+        http.begin(full_get_state.c_str());
+        int httpResponseCode = http.GET();
+          if (httpResponseCode == 200){
+//            int unit_state = httpGETReques/t(full_get_state);
+              String payload = http.getString();
+//              check_relay_state(payload);/
+              delay(50);
+              Serial.println(payload);
+              if (payload.equals("\"1\"")){
+                Serial.println("Relay HIGH");
+                digitalWrite(relay, HIGH);
+              }
+              if (payload.equals("\"0\"")){
+                Serial.println("Relay LOW");
+                digitalWrite(relay, LOW);
+              }
+              delay(50);
+              Serial.println(httpResponseCode);
+          }
+        
+        
+        http.end();
+      }else{
+        Serial.println("Wifi disconnected");
+      }
+      
+      last_time = millis();
+    }
+   
+    
+    delay(5000);//
 
   
 }
